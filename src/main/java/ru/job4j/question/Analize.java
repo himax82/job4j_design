efaf8d11bbc4ff63fab1f87d8cefb7bc2e1b3240
himax82 <himax82@gmail.com>
2021-08-28 19:23:04 +0800

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,20 @@
+package ru.job4j.question;
+
+import java.util.*;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Set<User> addSet = new HashSet<>(previous);
+        addSet.addAll(current);
+        Map<Integer, User > chMap = new HashMap<>();
+        for (User u : addSet) {
+            chMap.put(u.getId(), u);
+        }
+        int ch = addSet.size() - chMap.size();
+        int add = addSet.size() - previous.size() - ch;
+        int del = addSet.size() - current.size() - ch;
+        return new Info(add, ch, del);
+    }
+
+}
