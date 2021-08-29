58bc3b039baa6e72ad9d102ce4bf667c07612f60
himax82 <himax82@gmail.com>
2021-08-29 14:09:20 +0800

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,66 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+           String l;
+           while ((l = read.readLine()) != null) {
+               if (!l.startsWith(" ") && !l.startsWith("#") && !l.equals("")) {
+                   String[] ar = l.split("=");
+                   if (ar.length < 2 || ar[0].endsWith(" ") || ar[1].startsWith(" ")){
+                       throw new IllegalArgumentException();
+                   } else {
+                       values.put(ar[0], ar[1]);
+                   }
+               }
+           }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int size() {
+        return values.size();
+    }
+
+    public String value(String key) {
+        if (values.containsKey(key)) {
+            return values.get(key);
+        } else {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import ru.job4j.collection.SimpleQueue;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Maksim"));
+    }
+
+    @Test
+    public void whenPairWithCommentAndEmptyLine() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Maksim"));
+        assertThat(config.value("surname"), is("Pleskov"));
+        assertThat(config.size(), is(2));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenEmptyKey() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        config.value("fio");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotSample() {
+        String path = "./data/pair_with_not_sample.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
