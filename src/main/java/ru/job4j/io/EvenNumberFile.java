cea240c8db05935d033d82b99972be8280957249
himax82 <himax82@gmail.com>
2021-08-30 20:34:30 +0800

    0.2. FileInputStream [#4898 #195917]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,23 +4,34 @@ import java.io.FileInputStream;
 
 public class EvenNumberFile {
 
-    public static void main(String[] args) {
+    public String read(String path) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                if (Integer.parseInt(line) % 2 == 0) {
-                    System.out.println(line + " число четное");
-                } else {
-                    System.out.println(line + " число нечетное");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            return text.toString();
         }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+    public void printEven(String text) {
+        String[] lines = text.split(System.lineSeparator());
+        for (String line : lines) {
+            if (Integer.parseInt(line) % 2 == 0) {
+                System.out.println(line + " число четное");
+            } else {
+                System.out.println(line + " число нечетное");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenNumberFile readFile = new EvenNumberFile();
+        readFile.printEven(readFile.read("even.txt"));
     }
 }
