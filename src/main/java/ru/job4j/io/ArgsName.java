49b2b676fcde4877fc17e02883faf68099ba1269
himax82 <himax82@gmail.com>
2021-09-04 07:52:00 +0800

    5.2. Архивировать проект [#861 #197550]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +23,15 @@ public class ArgsName {
         for (String s : args) {
             String[] splits = s.split("=");
             if (splits.length != 2) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("USE SAMPLE KEY(-d, -e or -o)=ARGUMENT(PACK_FOLDER, EXCLUDE FILES AND INPUT_NAME_FILE)");
+            }
+            if(!splits[0].equals("-d") && !splits[0].equals("-e") && !splits[0].equals("-o")) {
+                throw new IllegalArgumentException("USE SAMPLE KEY(-d, -e or -o)");
+            }
+            if (splits[0].equals("-d")){
+                if(!Files.exists(Path.of(splits[1]))) {
+                    throw new IllegalArgumentException("FOLDER" + splits[1] + "DON'T EXIST");
+                }
             }
             values.put(splits[0].substring(1), splits[1]);
         }
@@ -47,6 +56,9 @@ public class ArgsName {
 
     public static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Usage java -jar zip.jar PACK_FOLDER, EXCLUDE FILES AND INPUT_NAME_FILE.");
+        }
         names.parse(args);
         return names;
     }
