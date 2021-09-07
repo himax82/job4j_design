9c15bb04367c7fe4fc1ab28a9fdc2d7cd23e858c
himax82 <80378936+himax82@users.noreply.github.com>
2021-09-07 21:13:44 +0800

    1. Схема прав пользователи и роли. [#1093]

--- create.sql
+++ create.sql
@@ -1,6 +1,51 @@
-create table books(
+create table item (
+	id serial primary key,
+	item text	
+);
+
+create table users (
 	id serial primary key,
 	name text,
-	author varchar(255),
-	pages integer
+	item_id int references item(id)
+);
+
+create table role (
+	id serial primary key,
+	role text,
+	users_id int references users(id)
+);
+
+create table rules (
+	id serial primary key,
+	rules text
+);
+
+create table role_rules_compose (
+	id serial primary key,
+	role_id int references role(id),
+	rules_id int references rules(id)
+);
+
+create table comments (
+	id serial primary key,
+	comment text,
+	item_id int references item(id)
+);
+
+create table attachs (
+	id serial primary key,
+	attachs text,
+	item_id int references item(id)
+);
+
+create table category (
+	id serial primary key,
+	category text,
+	item_id int references item(id)
+);
+
+create table state (
+	id serial primary key,
+	state text,
+	item_id int references item(id)
 );
--- insert.sql
+++ insert.sql
@@ -1,1 +1,26 @@
-insert into books (name, author, pages) values('Приключение Тома Соера', 'Марк Твен', 365);
+insert into item (item) values ('first');
+insert into item (item) values ('second');
+
+insert into users (name, item_id) values ('Ivan', 1);
+insert into users (name, item_id) values ('Petr', 2);
+
+insert into role (role, users_id) values ('Admin', 2);
+insert into role (role, users_id) values ('Users', 1);
+
+insert into rules (rules) values ('full');
+insert into rules (rules) values ('limited');
+
+insert into role_rules_compose (role_id, rules_id) values (1, 1);
+insert into role_rules_compose (role_id, rules_id) values (2, 2);
+
+insert into comments (coments, item_id) values ('tree', 1);
+insert into comments (coments, item_id) values ('log', 2);
+
+insert into attachs (attachs, item_id) values ('price', 1);
+insert into attachs (attachs, item_id) values ('factura', 1);
+
+insert into category (category, item_id) values ('urgent', 1);
+insert into category (category, item_id) values ('ordinary', 2);
+
+insert into state (state, item_id) values ('work', 1);
+insert into state (state, item_id) values ('pause', 1);
